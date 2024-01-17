ca1a8d01581484d4b6a8230b53cd781c25ea21d5
skippa-tf <skippa.tf2@gmail.com>
2024-01-16 20:36:53 -0800

    rename game

---
+++ src/main/java/Display.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Display {
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setTitle("Falling Sand Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(1280, 720);
+        window.setVisible(true);
+    }
+}
