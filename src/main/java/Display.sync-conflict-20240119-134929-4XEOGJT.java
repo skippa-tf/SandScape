573f8a63edd8afe35b4d7b80ce18ff6eab9aa815
skippa-tf <github@jasoncodes.ca>
2024-01-19 14:50:25 -0800

    update name

---
+++ src/main/java/Display.sync-conflict-20240119-134929-4XEOGJT.java
@@ -0,0 +1,12 @@
+import javafx.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+
+
+public class Display extends Application {
+/*
+    @Override
+    public void start(Stage primarystage){
+*/
+    }
+}
