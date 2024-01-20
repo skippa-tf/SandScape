8cb824a1a29b0930a61b0978d178e790422d07ed
skippa-tf <github@jasoncodes.ca>
2024-01-19 22:28:59 -0800

    update brushSize according to slider

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -8,14 +11,14 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.scene.layout.ColumnConstraints;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import javafx.beans.value.ChangeListener;
+
 
 
 public class Display extends Application {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
+    private int brushSize = 25;
     public static void main(String[] args) {
         Application.launch(args);
     }
@@ -25,31 +28,31 @@ public class Display extends Application {
     public void start(Stage primaryStage){
 
         // Assuming 'pane' is your GridPane
-        GridPane pane = new GridPane();
-        pane.setGridLinesVisible(false);
-        pane.setHgap(20); // Horizontal gap between grid cells
-        pane.setVgap(0); // Vertical gap between grid cells
+        GridPane controlsPane = new GridPane();
+        controlsPane.setGridLinesVisible(false);
+        controlsPane.setHgap(20); // Horizontal gap between grid cells
+        controlsPane.setVgap(0); // Vertical gap between grid cells
         for (int i = 0; i < 5; i++) {
             ColumnConstraints column = new ColumnConstraints();
             column.setPercentWidth(20);
             if (i == 5) {
                 column.setHalignment(HPos.RIGHT);
             }
-            pane.getColumnConstraints().add(column);
+            controlsPane.getColumnConstraints().add(column);
         }
 
         // Create our main functionality buttons
         Button clear = new Button("CLEAR");
-        pane.add(clear, 4, 2);
+        controlsPane.add(clear, 4, 2);
         GridPane.setHalignment(clear, HPos.RIGHT);
 
         Button start = new Button("START");
-        pane.add(start, 4, 0);
+        controlsPane.add(start, 4, 0);
         GridPane.setHalignment(start, HPos.RIGHT);
 
 
         Button pause = new Button("PAUSE");
-        pane.add(pause, 4, 1);
+        controlsPane.add(pause, 4, 1);
         GridPane.setHalignment(pause, HPos.RIGHT);
 
         // Radio buttons for material selection
@@ -58,49 +61,66 @@ public class Display extends Application {
         RadioButton stone = new RadioButton("Stone");
         stone.setToggleGroup(tg);
         stone.setSelected(true);
-        pane.add(stone, 1, 0);
+        controlsPane.add(stone, 1, 0);
 
         RadioButton sand = new RadioButton("Sand");
         sand.setToggleGroup(tg);
-        pane.add(sand, 2, 0);
+        controlsPane.add(sand, 2, 0);
 
         RadioButton water = new RadioButton("Water");
         water.setToggleGroup(tg);
-        pane.add(water, 3, 0);
+        controlsPane.add(water, 3, 0);
 
 
 
         RadioButton wood = new RadioButton("TBD");
         wood.setToggleGroup(tg);
-        pane.add(wood, 1, 1);
+        controlsPane.add(wood, 1, 1);
 
         RadioButton fire = new RadioButton("TBD");
         fire.setToggleGroup(tg);
-        pane.add(fire, 2, 1);
+        controlsPane.add(fire, 2, 1);
 
         RadioButton tbd = new RadioButton("TBD");
         tbd.setToggleGroup(tg);
-        pane.add(tbd, 3, 1);
+        controlsPane.add(tbd, 3, 1);
 
+        // Slider label && Text Alignment
+        Text strokeSliderLabel = new Text("Stroke Size: " + brushSize);
+        strokeSliderLabel.setTextAlignment(TextAlignment.CENTER);
+        controlsPane.add(strokeSliderLabel, 0, 0);
+        GridPane.setHalignment(strokeSliderLabel, HPos.CENTER);
 
         // Slider for stroke size selection
-        Slider strokeSlider = new Slider(0, 100, 25);
+        Slider strokeSlider = new Slider(0, 100, brushSize);
         strokeSlider.setShowTickLabels(true);
         strokeSlider.setMajorTickUnit(25);
         strokeSlider.setMinorTickCount(5);
         strokeSlider.setShowTickMarks(true);
-        pane.add(strokeSlider, 0, 1);
+        controlsPane.add(strokeSlider, 0, 1);
+        strokeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            // I got this code from: https://stackoverflow.com/a/22781451/23110483
+            // I don't know wtf is going on here.
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                brushSize = newValue.intValue();
+                strokeSliderLabel.setText("Stroke Size: " + brushSize);
+            }
+        });
 
-        // Slider label && Text Alignment
-        Text strokeSliderLabel = new Text("Stroke Size");
-        strokeSliderLabel.setTextAlignment(TextAlignment.CENTER);
-        pane.add(strokeSliderLabel, 0, 0);
-        GridPane.setHalignment(strokeSliderLabel, HPos.CENTER);
 
 
-        Scene scene = new Scene (pane, WIDTH, HEIGHT);
+        Scene scene = new Scene (controlsPane, WIDTH, HEIGHT);
         primaryStage.setTitle("SandScape");
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
+
+    public double getBrushSize(){
+        return brushSize;
+    }
+
+
+
+
 }
